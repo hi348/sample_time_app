7a5460d240653fba82bc385caf294384ae27e8ef
Harrysh <hi348@nyu.edu>
2020-02-19 20:49:24 -0500

    'assignment1'

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+import datetime as timmy
+from pytz import timezone
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return 'You were the Chosen One!!!'
+	
+@app.route('/time')
+def timm():
+    return  timmy.datetime.now(timezone('UTC')).astimezone(timezone('US/Eastern')).strftime("%H:%M")
 
 
 app.run(host='0.0.0.0',
